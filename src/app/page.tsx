0f4a10c69e27b4cb6d55035d7d13ea365ae3e28c
Patryk <patryk.d.janus@gmail.com>
2024-10-08 15:44:41 +0200

    add grid container styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-
 export default function Home() {
-  return (
-    <h1 className="text-red-400">Hello World</h1>
-  );
+	return <h1>Helo world</h1>
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,21 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-};
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			margin: {
+				50: '200px',
+			},
+			gridTemplateColumns: {
+				'12-72': 'repeat(12, 72px)',
+			},
+		},
+	},
+	plugins: [],
+}
 export default config;
