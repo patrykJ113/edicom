63687944a86ff1c5ee85d66b306312da24dcf527
Patryk <patryk.d.janus@gmail.com>
2024-10-08 20:52:35 +0200

    add roboto font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Roboto } from 'next/font/google'
 
 export const metadata: Metadata = {
 	title: 'Edicom',
 	description: 'Ecomarce site',
 }
+
+export const roboto = Roboto({
+	subsets: ['latin'],
+	display: 'swap',
+	weight: ['100', '300', '400', '400', '500', '700', '900'],
+})
 
 export default function RootLayout({
 	children,
@@ -13,7 +20,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body>
+			<body className={roboto.className}>
 				<main className='grid-layout'>{children}</main>
 			</body>
 		</html>
