ce80999faf64a6a7e70435049a5a17e2e458d434
Patryk <patryk.d.janus@gmail.com>
2024-10-14 10:18:19 +0200

    add gray color in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,6 +28,18 @@ const config: Config = {
 					800: '#1A2B8C',
 					900: '#14216B',
 				},
+				gray: {
+					50: '#FAFAFA',
+					100: '#F5F5F5',
+					200: '#EEEEEE',
+					300: '#E0E0E0',
+					400: '#BDBDBD',
+					500: '#9E9E9E',
+					600: '#757575',
+					700: '#616161',
+					800: '#424242',
+					900: '#212121',
+				},
 				page: '#F2F4FF', // Default color for the page backround
 			},
 			boxShadow: {
