8b6374334d78d498ed9bf974bdfe154b50660c08
Patryk <patryk.d.janus@gmail.com>
2024-10-23 22:27:53 +0200

    add description for checkbox test

--- src/__tests__/components/inputs/Checkbox.test.tsx
+++ src/__tests__/components/inputs/Checkbox.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import Checkbox from '@components/inputs/Checkbox'
 
-test('', () => {
+test('Check displays text', () => {
     const label = 'Hello'
     render(<Checkbox>{label}</Checkbox>)
 
