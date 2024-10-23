3ee8c6257161121e5c2e780ba2397b951af190d6
Patryk <patryk.d.janus@gmail.com>
2024-10-23 22:22:14 +0200

    fix checkbox path issue

--- src/app/components/inputs/Checkbox.tsx
+++ src/app/components/inputs/Checkbox.tsx
@@ -10,7 +10,7 @@ export default function Checkbox({ children }: Props) {
 				name='remember-me'
 				className='appearance-none text-white h-[1em] w-[1em] rounded-sm border border-brand-100
 					checked:border-brand checked:bg-brand
-					checked:bg-[url("../assets/svg/check.svg")] checked:bg-center
+					checked:bg-[url("/svg/check.svg")] checked:bg-center
 					checked:bg-no-repeat checked:bg-contain'
 			/>
 			<span className="font-semibold text-sm leading-5">{children}</span>
