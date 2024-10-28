267f5867bddb81b401f75dcd9ada920b220197d6
Patryk <patryk.d.janus@gmail.com>
2024-10-28 08:14:21 +0100

    add shallow test for Login and Registration forms ( implement it latter )

---
+++ src/__tests__/components/auth/RegisterForm.test.tsx
@@ -0,0 +1,16 @@
+describe('Register Form', () => {
+
+	describe('Validation', () => {
+		it('Password passes validation', () => {})
+		it('Password fails validation', () => {})
+		it('Email passes validation', () => {})
+		it('Email fails validation', () => {})
+	})
+
+	describe('Create User Request', () => {
+		it('User Created successfully', () => {})
+		it('Failed to create user', () => {})
+	})
+
+    it('Sing In link takes the user to the Login form', () => {})
+})
