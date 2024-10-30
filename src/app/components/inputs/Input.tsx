7e219cf724fb00c9eeff5acaeb94769e320ebbc5
Patryk <patryk.d.janus@gmail.com>
2024-10-30 19:50:57 +0100

    implement new input design

--- src/app/components/inputs/Input.tsx
+++ src/app/components/inputs/Input.tsx
@@ -4,12 +4,8 @@ import { useState } from 'react'
 
 type Props = {
 	label: string
-	large?: boolean
 	required?: boolean
-	hint?: string
-	error?: boolean
-	characterCount?: boolean
-	filled?: boolean
+	error?: string
 	name?: string
 	type?:
 		| 'button'
@@ -22,87 +18,67 @@ type Props = {
 		| 'submit'
 		| 'tel'
 		| 'text'
-	validateCallBack?: (str: string) => void
-	// icon: boolean !!! maybe add in the feature
 }
 
 export default function Input({
 	label,
 	required,
-	large,
-	characterCount,
-	hint,
 	error,
-	filled,
 	name,
 	type = 'text',
-	validateCallBack,
 }: Props) {
-	const maxLength = 100
+	const [isFocused, setIsFocused] = useState(false)
 	const [inputValue, setInputValue] = useState('')
+
+	const handleFocus = () => {
+		setIsFocused(true)
+	}
+
+	const handleBlur = () => {
+		setIsFocused(false)
+	}
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setInputValue(e.target.value)
-		if (validateCallBack) {
-			validateCallBack(e.target.value)
-		}
 	}
 
-	const isHintOnly = () => {
-		return !hint && characterCount && 'justify-end'
+	const getFocusStyle = () => {
+		return inputValue || isFocused ?
+				'text-xs leading-4 top-[2px]'
+			:	'text-base leading-6 top-1/2 -translate-y-1/2'
 	}
 
-	const getLableStyle = () => {
-		return large ? 'text-base leading-7' : 'text-sm leading-5'
+	const isError = () => {
+		return error !== '' && error
 	}
 
-	const getInputStyle = () => {
-		const height = large && 'h-14'
-		const err = error ? 'border-red-A400' : 'border-gray-500'
-		const borderAndBg =
-			filled ? 'rounded-t-[4px] bg-gray-200 border-b' : ' rounded-[4px] border'
-
-		return `${height} ${borderAndBg} ${err}`
-	}
-
-	const getTextColor = () => {
-		return error ? 'text-red-A400' : 'text-gray-700'
-	}
-
-	const isPassword = () => {
-		return type === 'password' ? 'true' : 'false'
+	const getErrorStyles = () => {
+		return isError() ? 'bg-red-50 border-red-A400' : ''
 	}
 
 	return (
 		<article className='flex flex-col gap-y-2'>
-			<h2 className={`font-normal ${getLableStyle()}`}>
-				{label}
-				{required && <span className='text-red-500'>*</span>}
-			</h2>
-			<input
-				type={type}
-				name={name}
-				required={required}
-				autoComplete={isPassword()}
-				className={`px-3 py-2 text-gray-700 outline-none text-base leading-6 ${getInputStyle()}`}
-				onChange={handleChange}
-			/>
-			<div className={`px-3 flex ${isHintOnly()}`}>
-				{hint && (
-					<span
-						className={`text-sm font-normal leading-5 flex-1 ${getTextColor()}`}
-					>
-						{hint}
-					</span>
-				)}
-				{characterCount && (
-					<span
-						className={`pl-3 text-sm font-normal leading-5 ${getTextColor()}`}
-					>
-						{inputValue.length}/{maxLength}
-					</span>
-				)}
-			</div>
+			<label
+				className={`border-gray-300 border-2 px-4 pt-6 pb-1 relative focus-within:border-black
+					rounded ${getErrorStyles()}`}
+			>
+				<span
+					className={`text-gray-600 absolute left-4 transition-all duration-150 ${getFocusStyle()}`}
+				>
+					{label}
+				</span>
+				<input
+					type={type}
+					name={name}
+					required={required}
+					autoComplete='true'
+					className='bg-transparent outline-none text-base leading-6'
+					onChange={handleChange}
+					onFocus={handleFocus}
+					onBlur={handleBlur}
+				/>
+			</label>
+			{isError() && <p className='text-sm leading-5 text-red-A400 pl-4'>{error}</p>}
 		</article>
 	)
 }
