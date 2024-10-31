dd736171d5a243ed87443543e3f20abcc5887105
Patryk <patryk.d.janus@gmail.com>
2024-10-31 20:03:06 +0100

    rewrite tests for new input component

--- src/__tests__/components/inputs/Input.test.tsx
+++ src/__tests__/components/inputs/Input.test.tsx
@@ -1,193 +1,138 @@
-import { render, screen } from '@testing-library/react'
+import { cleanup, render, screen } from '@testing-library/react'
 import Input from '@/app/components/inputs/Input'
 import userEvent from '@testing-library/user-event'
 
-test('Input displays label', () => {
-	const label = 'Name'
-	render(<Input label={label} />)
-	screen.getByRole('heading', { name: new RegExp(label, 'i') })
-})
+jest.mock('@svg/eye.svg', () => 'svg')
 
-test('Displayes * if input is required', () => {
-	const label = 'Name'
-	const { rerender } = render(<Input label={label} required />)
+describe('Input', () => {
+	describe('Label', () => {
+		it('Input displays label', () => {
+			const { container } = render(<Input label='Name' />)
+			const label = container.querySelector('label')
+			const span = label?.querySelector('span')
 
-	let h2 = screen.getByRole('heading', {
-		name: new RegExp(label, 'i'),
-		level: 2,
+			expect(span).toBeInTheDocument()
+			expect(span).toHaveTextContent('Name')
+		})
+
+		it('Label changes sizes plus moves up or down when input is focused or blurred', async () => {
+			const { container } = render(<Input label='Name' />)
+			const label = container.querySelector('label')
+			const span = label?.querySelector('span')
+			const input = screen.getByRole('textbox')
+
+			expect(span).toHaveClass('text-base leading-6 top-1/2 -translate-y-1/2')
+
+			await userEvent.click(input)
+
+			expect(span).toHaveClass('text-xs leading-4 top-2')
+
+			await userEvent.tab()
+
+			expect(span).toHaveClass('text-base leading-6 top-1/2 -translate-y-1/2')
+		})
+
+		it('Label stays in place when the input has a value in it', async () => {
+			const { container } = render(<Input label='Name' />)
+			const label = container.querySelector('label')
+			const span = label?.querySelector('span')
+			const input = screen.getByRole('textbox')
+
+			await userEvent.type(input, 'Hello')
+			await userEvent.tab()
+
+			expect(span).toHaveClass('text-xs leading-4 top-2')
+
+			await userEvent.clear(input)
+			await userEvent.tab()
+
+			expect(span).toHaveClass('text-base leading-6 top-1/2 -translate-y-1/2')
+		})
 	})
 
-	let span = h2.querySelector('span')
-	expect(span).toBeInTheDocument()
-	expect(span).toHaveTextContent('*')
+	it('Required attribute is added to the html element', () => {
+		const { rerender } = render(<Input label='Name' required />)
+		const input = screen.getByRole('textbox')
 
-	rerender(<Input label={label} />)
+		expect(input).toHaveAttribute('required')
 
-	h2 = screen.getByRole('heading', {
-		name: new RegExp(label, 'i'),
-		level: 2,
+		rerender(<Input label='Name' />)
+
+		expect(input).not.toHaveAttribute('required')
 	})
 
-	span = h2.querySelector('span')
-	expect(span).not.toBeInTheDocument()
-})
+	describe('Error', () => {
+		it('Input hasa a red outline and background color', () => {
+			const { rerender, container } = render(
+				<Input label='Name' error='error occurred' />,
+			)
 
-test('Renders larger input when the large option is set', () => {
-	const label = 'Name'
-	const { rerender } = render(<Input label={label} large />)
+			const label = container.querySelector('label')
 
-	let h2 = screen.getByRole('heading', {
-		name: new RegExp(label, 'i'),
-		level: 2,
+			expect(label).toHaveClass('bg-red-50 border-red-A400')
+
+			rerender(<Input label='Name' />)
+
+			expect(label).not.toHaveClass('bg-red-50 border-red-A400')
+		})
+
+		it('Hint is displayed with the error message bellow the input', () => {
+			const { rerender } = render(<Input label='Name' error='error occurred' />)
+
+			const hint = screen.getByText(/error occurred/i)
+
+			rerender(<Input label='Name' />)
+
+			expect(hint).not.toBeInTheDocument()
+		})
 	})
 
-	let input = screen.getByRole('textbox')
+	it('Name attribute is applied to the input when name prop is specified', () => {
+		const { rerender } = render(<Input label='Name' name='Name' />)
+		const input = screen.getByRole('textbox')
 
-	expect(h2).toHaveClass('text-base leading-7')
-	expect(input).toHaveClass('h-14')
+		expect(input).toHaveAttribute('name', 'Name')
 
-	rerender(<Input label={label} />)
+		rerender(<Input label='Name' />)
 
-	h2 = screen.getByRole('heading', {
-		name: new RegExp(label, 'i'),
-		level: 2,
+		expect(input).not.toHaveAttribute('name')
 	})
 
-	input = screen.getByRole('textbox')
+	it('Type attribute is applied to the input when type prop is specified', () => {
+		const types: [
+			'file',
+			'email',
+			'image',
+			'number',
+			'password',
+			'tel',
+			'text',
+			'date',
+		] = ['file', 'email', 'image', 'number', 'password', 'tel', 'text', 'date']
 
-	expect(h2).not.toHaveClass('text-base leading-7')
-	expect(input).not.toHaveClass('h-14')
-})
+		types.forEach(type => {
+			cleanup()
+			const { container } = render(<Input label={'Name'} type={type} />)
 
-test('Displays hint', () => {
-	const label = 'Name'
-	const hint = 'Some Hint'
-	const { rerender } = render(<Input label={label} hint={hint} />)
-
-	screen.getByText(new RegExp(hint, 'i'))
-
-	rerender(<Input label={label} />)
-	expect(screen.queryByText(new RegExp(hint, 'i'))).not.toBeInTheDocument()
-})
-
-test('Displays character count', () => {
-	const { rerender } = render(<Input label={'Name'} characterCount />)
-
-	let atricle = screen.getByRole('article')
-	let characterCount = atricle.querySelector('div > span')
-	expect(characterCount).toBeInTheDocument()
-
-	rerender(<Input label={'Name'} />)
-
-	atricle = screen.getByRole('article')
-	characterCount = atricle.querySelector('div > span')
-	expect(characterCount).not.toBeInTheDocument()
-})
-
-test('Displays character count changes on input', async () => {
-	render(<Input label={'Name'} characterCount />)
-
-	const input = screen.getByRole('textbox')
-	const atricle = screen.getByRole('article')
-	const characterCount = atricle.querySelector('div > span')
-
-	expect(characterCount).toHaveTextContent('0/100')
-
-	let inputTest = 'Hello'
-	await userEvent.type(input, inputTest)
-
-	expect(characterCount).toHaveTextContent(`${inputTest.length}/100`)
-
-	inputTest = 'Hello World'
-	await userEvent.clear(input)
-	await userEvent.type(input, inputTest)
-	expect(characterCount).toHaveTextContent(`${inputTest.length}/100`)
-
-	await userEvent.clear(input)
-	expect(characterCount).toHaveTextContent('0/100')
-})
-
-test('Applies error styles when the error prop is true', () => {
-	const { rerender } = render(
-		<Input label={'Name'} hint={'Some Hint'} characterCount />,
-	)
-
-	const atricle = screen.getByRole('article')
-	let input = screen.getByRole('textbox')
-	let hint = atricle.querySelector('div > span:first-child')
-	let characterCount = atricle.querySelector('div > span:last-child')
-
-	expect(input).toHaveClass('border-gray-500')
-	expect(hint).toHaveClass('text-gray-700')
-	expect(characterCount).toHaveClass('text-gray-700')
-
-	rerender(<Input label={'Name'} hint={'Some Hint'} characterCount error />)
-
-	input = screen.getByRole('textbox')
-	hint = atricle.querySelector('div > span:first-child')
-	characterCount = atricle.querySelector('div > span:last-child')
-
-	expect(input).toHaveClass('border-red-A400')
-	expect(hint).toHaveClass('text-red-A400')
-	expect(characterCount).toHaveClass('text-red-A400')
-})
-
-test('Renders a filled input when the filled atribute is provided', () => {
-	render(<Input label={'Name'} filled />)
-
-	const input = screen.getByRole('textbox')
-	expect(input).toHaveClass('rounded-t-[4px] bg-gray-200 border-b')
-})
-
-test('the name is applied to the input', () => {
-	render(<Input label={'Name'} name='some-name' />)
-	const input = screen.getByRole('textbox')
-
-	expect(input).toHaveAttribute('name', 'some-name')
-})
-
-test('check if tye type is set corectly', () => {
-	const types: [
-		'button',
-		'email',
-		'file',
-		'image',
-		'number',
-		'password',
-		'submit',
-		'tel',
-		'text',
-		'date'
-	] = [
-		'button',
-		'email',
-		'file',
-		'image',
-		'number',
-		'password',
-		'submit',
-		'tel',
-		'text',
-		'date'
-	]
-
-	const { rerender } = render(<></>)
-
-	types.forEach(type => {
-		rerender(<Input label={'Name'} type={type} />)
-
-		const input = document.querySelector(`input[type=${type}]`)
-
-		expect(input).toHaveAttribute('type', type);
+			const input = container.querySelector('input')
+			expect(input).toBeInTheDocument()
+			expect(input).toHaveAttribute('type', type)
+		})
 	})
-})
 
-test('validateCallBack is called when typing in the input', async () => {
-	const validateCallBack = jest.fn()
+	it('The password visibility is toggled when clicking the eye icons', async () => {
+		const { container } = render(<Input label='Name' type='password' />)
+		const showPassword = screen.getByRole('button') 
+		const input = container.querySelector('input[type="password"]')
 
-	render(<Input label={'Name'} validateCallBack={validateCallBack} />)
-	const input = screen.getByRole('textbox')
-	await userEvent.type(input, 'Hello')
+		expect(input).toBeInTheDocument();
 
-	expect(validateCallBack).toHaveBeenCalledTimes('Hello'.length)
+		await userEvent.click(showPassword)
+
+		expect(input).toHaveAttribute('type', 'text')
+
+		await userEvent.click(showPassword)
+		expect(input).toHaveAttribute('type', 'password')
+
+	})
 })
