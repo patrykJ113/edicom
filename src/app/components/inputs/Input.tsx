5d7e728a76c2282f554c9260100af71b74472626
Patryk <patryk.d.janus@gmail.com>
2024-10-31 15:25:28 +0100

    add eye button to toggle the password visibility

--- src/app/components/inputs/Input.tsx
+++ src/app/components/inputs/Input.tsx
@@ -1,6 +1,8 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import Eye from '@svg/eye.svg'
+import EyeOpened from '@svg/eye-opened.svg'
 
 type Props = {
 	label: string
@@ -29,6 +31,8 @@ export default function Input({
 }: Props) {
 	const [isFocused, setIsFocused] = useState(false)
 	const [inputValue, setInputValue] = useState('')
+	const [showPassword, setShowPassword] = useState(false)
+	const [inputType, setInputType] = useState(type)
 
 	const handleFocus = () => {
 		setIsFocused(true)
@@ -44,7 +48,7 @@ export default function Input({
 
 	const getFocusStyle = () => {
 		return inputValue || isFocused ?
-				'text-xs leading-4 top-[2px]'
+				'text-xs leading-4 top-2'
 			:	'text-base leading-6 top-1/2 -translate-y-1/2'
 	}
 
@@ -52,15 +56,28 @@ export default function Input({
 		return error !== '' && error
 	}
 
+	const isPassword = () => {
+		return type === 'password'
+	}
+
 	const getErrorStyles = () => {
 		return isError() ? 'bg-red-50 border-red-A400' : ''
 	}
 
+	const togglePwdVisibility = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault()
+		setShowPassword(!showPassword)
+	}
+
+	useEffect(() => {
+		setInputType(showPassword ? 'text' : 'password')
+	}, [showPassword])
+
 	return (
 		<article className='flex flex-col gap-y-2'>
 			<label
-				className={`border-gray-300 border-2 px-4 pt-6 pb-1 relative focus-within:border-black
-					rounded ${getErrorStyles()}`}
+				className={`border-gray-300 border-2 relative focus-within:border-black rounded
+					${getErrorStyles()}`}
 			>
 				<span
 					className={`text-gray-600 absolute left-4 transition-all duration-150 ${getFocusStyle()}`}
@@ -68,17 +85,34 @@ export default function Input({
 					{label}
 				</span>
 				<input
-					type={type}
+					type={inputType}
 					name={name}
 					required={required}
-					autoComplete='true'
-					className='bg-transparent outline-none text-base leading-6'
+					className='bg-transparent outline-none text-base leading-6 pl-4 pr-12 pt-6 pb-1 w-full'
+					autoComplete='off'
 					onChange={handleChange}
 					onFocus={handleFocus}
 					onBlur={handleBlur}
 				/>
+				<button
+					onClick={togglePwdVisibility}
+					className={!isPassword() ? 'hidden' : ''}
+				>
+					{showPassword ?
+						<EyeOpened
+							className='absolute right-4 top-1/2 -translate-y-1/2 fill-gray-600 cursor-pointer
+								hover:fill-gray-800 h-6 w-6'
+						/>
+					:	<Eye
+							className='absolute right-4 top-1/2 -translate-y-1/2 fill-gray-600 cursor-pointer
+								hover:fill-gray-800 h-6 w-6'
+						/>
+					}
+				</button>
 			</label>
-			{isError() && <p className='text-sm leading-5 text-red-A400 pl-4'>{error}</p>}
+			{isError() && (
+				<p className='text-sm leading-5 text-red-A400 pl-4'>{error}</p>
+			)}
 		</article>
 	)
 }
