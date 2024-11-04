0de0479707a144d07d7c1cb958a786b6f2b6802a
Patryk <patryk.d.janus@gmail.com>
2024-11-04 17:54:32 +0100

    add correct bg color for auth routes

--- src/app/[locale]/auth/layout.tsx
+++ src/app/[locale]/auth/layout.tsx
@@ -1,7 +1,22 @@
+/* eslint-disable @next/next/no-async-client-component */
+'use client'
+
+import { useEffect } from "react";
+
+
 export default async function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode
 }>) {
+	useEffect(() => {
+	document.body.classList.add('bg-brand-100')
+
+    return () => {
+	document.body.classList.remove('bg-brand-100')
+
+    };
+  }, []);
+  
 	return <main className='grid-layout h-screen items-center'>{children}</main>
 }
